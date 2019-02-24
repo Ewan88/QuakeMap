68d0c64dd807432750c742e78bd27a59830830b3
Ewan88 <baileyewan@gmail.com>
2019-02-24 15:31:27 +0000

    adds api and components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div id="ui">
+      <div id="form">
+        <label for="from">from:</label>
+        <input id="from" type="date" v-model="from"/>
+
+        <label for="to">to:</label>
+        <input id="to" type="date" v-model="to"/>
+
+        <label for="mag">mag:</label>
+        <input id="mag" type="number" v-model="mag" max="10" min="0"/>
+
+        <button v-on:click="getEvents">
+          search
+        </button>
+      </div>
+
+      <div id="drop-down">
+        <event-list :events="events"/>
+      </div>
+
+      <div id="detail" v-if="selectedEvent">
+        <event-detail :event="selectedEvent"/>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EventList from './components/EventList.vue';
+import EventDetail from './components/EventDetail.vue';
+import { eventBus } from './main.js';
 
 export default {
   name: 'app',
+  data(){
+    return {
+      from: '2019-02-12',
+      to: '2019-02-22',
+      mag: '5',
+      events: {},
+      selectedEvent: null,
+    };
+  },
   components: {
-    HelloWorld
+    "event-list": EventList,
+    "event-detail": EventDetail,
+  },
+  mounted(){
+    this.getEvents();
+  },
+  methods: {
+    getEvents(){
+      fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${this.from}&endtime=${this.to}&minmagnitude=${this.mag}`)
+      .then(res => res.json())
+      .then(events => this.events = events)
+
+      eventBus.$on('selected-event', (event) => {
+        this.selectedEvent = event;
+      });
+    },
   }
 }
 </script>
---
+++ src/components/EventDetail.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <p>{{event.properties.place}}</p>
+    <p>{{event.properties.mag}} mb</p>
+    <p>{{event.geometry.coordinates[2]}} km</p>
+    <p>
+      {{event.geometry.coordinates[0]}}
+      {{event.geometry.coordinates[1]}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event-detail",
+  props: ["event"]
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
